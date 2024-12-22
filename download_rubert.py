dac7484a2ca53e368df0d552c0e4bf098080ce49
JartiX <artempilavin@gmail.com>
2024-12-22 22:56:33 +0800

    fix

--- download_rubert.py
+++ download_rubert.py
@@ -1,10 +1,10 @@
-from transformers import BertTokenizer, BertModel
+from transformers import AutoTokenizer, TFAutoModel
 
 from ENV_VARS import MODEL_NAME, RUBERT_MODEL_PATH, RUBERT_TOKENIZER_PATH
 
 
-tokenizer = BertTokenizer.from_pretrained(MODEL_NAME)
-model = BertModel.from_pretrained(MODEL_NAME, from_pt=True)
+tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+model = TFAutoModel.from_pretrained(MODEL_NAME, from_pt=True)
 
 tokenizer.save_pretrained(RUBERT_TOKENIZER_PATH)
 model.save_pretrained(RUBERT_MODEL_PATH)
